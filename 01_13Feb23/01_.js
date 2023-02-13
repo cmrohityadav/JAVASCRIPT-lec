a789f1f04f5ca6b41a11d58fbfaa9fe7b69a5d80
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-13 20:48:45 +0530

    js basic ==> variable and operator

---
+++ 01_13Feb23/01_.js
@@ -0,0 +1,75 @@
+// value and datatype 
+// primitive Data Type
+
+// Number:2,3,4,1.5,24
+// -String: "rohit" or 'rahul'
+// -Boolean: true or false 
+// -null nothing 
+// -undefined
+
+
+// Non-primitive Data Types 
+// -Array 
+let names=["rohit","rahul","piysuh",false,2,1,2.5]
+
+// -Object:{}
+
+// browser event and Object is very important
+
+
+// always use let and const
+
+
+// Variable
+let highScore=300
+console.log(highScore)
+
+let pwSkillsCoursePrice=3500
+console.log(pwSkillsCoursePrice)
+
+pwSkillsCoursePrice=2000
+console.log(pwSkillsCoursePrice)
+
+let courseGatePrice=400;
+let gstOnCourses=36;
+let finalPurchaseAmout=courseGatePrice + gstOnCourses;
+console.log(finalPurchaseAmout);
+console.log("final amount to be paid:",finalPurchaseAmout);
+
+// arithmetic
+let pwDsaCourse=500;
+console.log("final price of dsa Course", pwDsaCourse + gstOnCourses);
+
+console.log("Ans is :", 5%2) //modulo
+console.log("Ans2 is :", 5/2)
+console.log("Ans3 is :", 5/2)
+console.log("Ans3 is :", 5**2) //5 to the power 2
+
+
+
+// comaparition
+let rahulHighScore=100;
+let rohitHighScore=200;
+let rohityHighScore="200";
+
+console.log(rahulHighScore==rohitHighScore);
+console.log(rahulHighScore>rohitHighScore);
+console.log(rahulHighScore<rohitHighScore);
+console.log("dusra");
+console.log(rohityHighScore<rohitHighScore);
+console.log(rohityHighScore==rohitHighScore);
+console.log(rohityHighScore===rohitHighScore);
+
+console.log("logic");
+let value1=true
+let value2=false
+let value3=true
+let value4=false
+
+console.log(value1 && value2);
+console.log(value1 || value2);
+
+
+let gmailAccount=true
+let mobileAccount=false;
+console.log(gmailAccount || mobileAccount);
