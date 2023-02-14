f03e211b327ffa90e40d5c81651dec6003d78b40
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-15 00:14:27 +0530

    conditionals

---
+++ 02_14Feb23/03_conditional.js
@@ -0,0 +1,14 @@
+let temperature;
+
+temperature=39;
+
+if(temperature<20){
+    console.log("it's very cold outside");
+}
+
+if(temperature<30){
+    console.log("its moderate");
+}
+else{
+    console.log("its really hot outside");
+}
