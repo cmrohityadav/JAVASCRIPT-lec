9972402d8ce4c525cd3f8b11f85672dcd3d9752c
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-16 01:01:31 +0530

    conditional

---
+++ 03_15Feb23/01_conditions.js
@@ -0,0 +1,24 @@
+let age = 13;
+if (age >= 18) {
+    console.log("You are allowed");
+}
+else {
+    console.log("You are not allowed");
+}
+
+
+let signal = "yellow"
+
+if (signal == "red") {
+    console.log("Stop");
+}
+else if(signal=="yellow"){
+    console.log("go slow");
+}
+// even this condition is true but not execute
+else if (signal == "yellow") {
+    console.log("udjfjodo");
+}
+else {
+    console.log("Go");
+}
