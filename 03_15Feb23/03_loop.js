61f617743ebb38551e0a3e08218cc2c1049b7c2d
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-16 01:01:48 +0530

    loop

---
+++ 03_15Feb23/03_loop.js
@@ -0,0 +1,30 @@
+
+// for loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// while (init, Conditon, inc/dec)
+
+let j = 0;
+while (j < 5) {
+  console.log("121");
+  j++;
+}
+
+console.log()
+console.log()
+let i = 0;
+do {
+  console.log("Hello World");
+  i++;
+} while (i < 5);
+
+
+
+
+// Ternary Op / Ternay Conditon
+//  condtion ? TRUE:False
+
+isloggedin = false;
+isloggedin ? console.log("Buying Mer") : console.log("Not Buying");
