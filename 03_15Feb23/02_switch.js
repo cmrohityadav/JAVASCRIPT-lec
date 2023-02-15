c9e2f58ec1d4e5eda71ed887e4c01602c310ffca
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-16 01:01:40 +0530

    switch

---
+++ 03_15Feb23/02_switch.js
@@ -0,0 +1,17 @@
+
+let user = "Admin";
+switch (user) {
+  case "Admin":
+    console.log("He is Admin");
+    break;
+
+  case "Student":
+    console.log("He is Student");
+    break;
+
+  case "Mentor":
+    console.log("He is Mentor");
+    break;
+  default:
+    console.log("I am Default");
+}
