0b8d2ce004e6d7758af7ae0227a699494ef8de45
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-21 15:25:23 +0530

    basic array

---
+++ 06_Array/01_arrayBasics.js
@@ -0,0 +1,14 @@
+let arr=[1,2,3,"rohit","yadav",false];
+console.log(arr);
+
+let arr2=new Array();
+console.log(arr2); //it will print blank array
+
+let arr3=new Array(5,6);
+console.log(arr3);
+console.log(arr[4]);
+arr[3]="Rahul"; //array is mutable in js==>element ko change kr skte hai
+
+console.log(arr);
+
+
