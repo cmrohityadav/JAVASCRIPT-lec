84b0d980d4c541858b2fbf2d80d2fa1d17a72968
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-21 15:47:46 +0530

    basics of functions

---
+++ 07_Function/01_basicFunction.js
@@ -0,0 +1,18 @@
+function greet(){
+    console.log("hello rohit welcome");
+
+}
+
+function square(x){ //x=>> parameter
+    console.log(x*x);
+}
+function sum(x,y){
+    return x+y;
+}
+greet();
+square(8); //8=>> argument
+
+let a=sum(8,10);
+console.log(a);
+
+
