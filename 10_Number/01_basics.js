79287c4f7d9e7f6d85aa3975a5fc2c8f637903d5
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-02-25 20:50:15 +0530

    basics of number in js

---
+++ 10_Number/01_basics.js
@@ -0,0 +1,20 @@
+const score=400;
+console.log(score)
+console.log(typeof(score));
+
+const balance= new Number(100);
+console.log(balance);
+console.log(typeof(balance));
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // .(point) k baad kitna zero lagega
+
+const otherNumber=123.8966;
+console.log(otherNumber.toPrecision(4));
+
+
+const hundreds=10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
