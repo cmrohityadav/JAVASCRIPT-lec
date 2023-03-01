d8424e627ce1151cad584db3442387897898bd27
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-03-01 20:38:38 +0530

    date and time in js

---
+++ 05_20Feb23/01_date.js
@@ -0,0 +1,23 @@
+let mydate=new Date();
+console.log(mydate);
+console.log("to string")
+console.log(mydate.toString());
+
+console.log("todatestring")
+console.log(mydate.toDateString());
+
+console.log("toisostring")
+console.log(mydate.toISOString());
+
+console.log("toLocalDateString")
+console.log(mydate.toLocaleDateString());
+
+console.log("tolocalString")
+console.log(mydate.toLocaleString())
+
+console.log(typeof mydate);
+
+
+// let myCreatedDate=new Date(2025,0,23);
+let myCreatedDate=new Date("01-22-2024");
+console.log(myCreatedDate.toDateString())
---
+++ 05_20Feb23/02_timeStamp.js
@@ -0,0 +1,14 @@
+let myTimeStamp=Date.now();
+
+console.log(myTimeStamp);
+
+let myDate=new Date("03-01-2023");
+console.log(myDate.toLocaleString());
+console.log(myDate.getTime());
+
+
+console.log(Math.round(Date.now()/1000));
+
+console.log(myDate.getTime());
+console.log(myDate.getDay());
+console.log(myDate.getMonth()+1);
