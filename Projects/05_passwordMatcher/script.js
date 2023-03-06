a1f9db2cd30a415c9fc90c546812fb7129c3b571
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-03-07 03:15:56 +0530

    version 2

--- Projects/05_passwordMatcher/script.js
+++ Projects/05_passwordMatcher/script.js
@@ -1,20 +1,25 @@
 
-let submit=document.getElementById("submit")
-submit.addEventListener("click",myFun);
-function myFun(){
-    let x=document.getElementById("password").value;
-    
-    let y=document.getElementById("conpassword").value;
-    if(x==y){
-        document.getElementById("display").innerText="Match";
-        document.getElementById("display").style.backgroundColor='green';
-        
-        
-        
+let submit = document.getElementById("submit")
+submit.addEventListener("click", myFun);
+function myFun() {
+    let x = document.getElementById("password").value;
+
+    let y = document.getElementById("conpassword").value;
+    if (x == y) {
+        // document.getElementById("display").innerText="Match";
+        document.getElementById("display").style.backgroundColor = 'green';
+        const para = document.createElement("p");
+        para.innerHTML = "Match";
+        document.getElementById("display").appendChild(para);
+
+
     }
-    else{
-        document.getElementById("display").innerText=" Not match";
-        document.getElementById("display").style.backgroundColor='Red';
+    else {
+        // document.getElementById("display").innerText = " Not match";
+        document.getElementById("display").style.backgroundColor = 'Red';
+        const para = document.createElement("p");
+        para.innerHTML = "Not Match";
+        document.getElementById("display").appendChild(para);
     }
 
 
