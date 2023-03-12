4e86fee8513e0c671edf67b81486f929984108b4
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-03-12 21:56:59 +0530

    array

---
+++ Projects/06_lightOnOff/script.js
@@ -0,0 +1,19 @@
+let btn=document.getElementById("btn");
+let bulb=document.getElementById("bulb");
+console.log(btn.textContent);
+
+btn.addEventListener("click",onclickBtn);
+function onclickBtn(element){
+    if(btn.textContent.includes("On")){
+        bulb.src="./assets/Light-Bulb-On.jpg";
+        btn.textContent="Turn Off";
+        btn.style.backgroundColor="red";
+        console.log(element)
+
+    }else{
+        bulb.src="./assets/Light-Bulb-Off.jpg";
+        btn.textContent="Turn On"; 
+        btn.style.backgroundColor="#6bf36b";
+        
+    }
+}
