b085d2687abbd060554641957ca933f667c0203a
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-03-27 20:02:38 +0530

    for each

---
+++ 05_Revision/02_loop/04_forEach.js
@@ -0,0 +1,20 @@
+console.log("using   function")
+
+arr=[1,2,3,4,5];
+arr.forEach((elementHai,index,arraybhai)=>{
+ console.log(elementHai,index,arraybhai)
+})
+
+
+console.log("using  standard function")
+arr.forEach(function(elementHai,index,arraybhai){
+    console.log(elementHai,index,arraybhai)
+   })
+ 
+   
+
+//
+ar=["roHit","yaDav","kiRan"];
+ar.forEach((ele)=>{
+    console.log(ele.toUpperCase())
+})
