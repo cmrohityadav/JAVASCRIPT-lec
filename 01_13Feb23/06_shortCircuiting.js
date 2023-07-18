4b3f665d7a36b69f88405815f04caee0d9f115dc
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-18 19:59:34 +0530

    short circuiting

---
+++ 01_13Feb23/06_shortCircuiting.js
@@ -0,0 +1,20 @@
+/*
+
+What values are the falsy in js?
+
+null
+undefined
+"" :empty String
++0,-0 ,NaN
+false
+
+
+
+*/
+
+console.log(6&&10);
+console.log(60&&10);
+console.log(6||0);
+console.log(0||6);
+console.log(0||-0);
+
