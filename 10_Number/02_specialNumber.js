0d80722b59382876c8a8dd5d5940b0a68a2856f2
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-18 20:20:32 +0530

    special number

---
+++ 10_Number/02_specialNumber.js
@@ -0,0 +1,30 @@
+/*
+
+0
+
+
+
+-0 :directional quantity
+
+
+
+NaN: Not a Number
+
+#interview question: which is the only number in js not equal to itself =>NaN
+
+
+*/
+
+console.log(10/null);
+console.log(undefined/null);
+console.log(NaN==NaN)
+console.log(NaN===NaN)
+
+
+//special number
+console.log(+0)
+console.log(-0)
+console.log(NaN)
+console.log(Infinity);
+console.log(-Infinity);
+console.log(10<Infinity);
