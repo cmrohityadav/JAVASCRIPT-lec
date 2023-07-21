b3ae00ad8e5322d5adcadae17f6fb5c6402ee5b8
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-21 17:02:54 +0530

    type of triangle

---
+++ 02_ConditonalStatements/16_typeOfTriangle.js
@@ -0,0 +1,13 @@
+let a=7;
+let b=7;
+let c=11;
+
+if(a==b && b==c && c==a){
+    console.log(`its is a equilateral triangle`);
+}else if(a!=b && b!=c && a!=c){
+    console.log(`its is a  isosceles triangle`);
+
+}else{
+    console.log(`its is a scelene triangle`);
+
+}
