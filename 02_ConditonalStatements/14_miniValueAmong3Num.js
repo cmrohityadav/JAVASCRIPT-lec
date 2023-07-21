8de841c59f071f4b78af0996de1559e285c55cfb
cmrohityadav <cmrohityadav23@gmail.com>
2023-07-21 16:05:57 +0530

    min among 3

---
+++ 02_ConditonalStatements/14_miniValueAmong3Num.js
@@ -0,0 +1,15 @@
+let val1=0;
+let val2=45;
+let val3=4;
+
+
+if(val1<val2 && val1<val3){
+    console.log(`the mini value is ${val1}`);
+
+}else if(val2<val1 && val2<val3){
+    console.log(`the mini value is ${val2}`);
+  
+}else{
+    console.log(`the mini value is ${val3}`);
+
+}
