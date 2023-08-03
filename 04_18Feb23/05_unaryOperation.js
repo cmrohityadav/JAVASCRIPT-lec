5293ca020a0dcbcc87b177713824d33de1c5032c
cmrohityadav <cmrohityadav23@gmail.com>
2023-08-03 19:56:58 +0530

    unary optr

---
+++ 04_18Feb23/05_unaryOperation.js
@@ -0,0 +1,16 @@
+let x=10;
+let b=x++;
+console.log(x,b)
+
+let c=100
+let d=++c;
+console.log(c,d)
+
+
+let y=1000;
+let r=y--;
+console.log(y,r)
+
+let g=200;
+let z=++g;
+console.log(g,z)
---
+++ 04_18Feb23/06_unaryPlus.js
@@ -0,0 +1,10 @@
+let x="22";
+let y=+x;
+
+console.log(typeof x)
+console.log(typeof y)
+
+let z=-x;
+console.log(typeof z)
+console.log( z)
+
