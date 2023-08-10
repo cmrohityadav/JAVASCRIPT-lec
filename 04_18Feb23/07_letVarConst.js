c7bf6801b4b18c32b4072581032b8cb48d135380
cmrohityadav <cmrohityadav23@gmail.com>
2023-08-10 21:49:49 +0530

    let var and const

---
+++ 04_18Feb23/07_letVarConst.js
@@ -0,0 +1,27 @@
+// const aadhar=101;
+// aadhar=1010;
+// console.log(aadhar)
+//  o/p : TypeError: Assignment to constant variable.
+
+
+let marks=10;
+marks=20;
+console.log(marks)
+
+
+var name="Rohit";
+let surname="yadav";
+{
+    console.log(name);
+  console.log(surname);
+  var clg="ACE";
+  let city="67";
+  console.log(clg);
+  console.log(city);
+
+
+  
+    
+}
+console.log(clg);
+// console.log(city);  ==>>>error
