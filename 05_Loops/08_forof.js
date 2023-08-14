662f1bdd142444f51aae2480aa49cdd1d9e58af8
cmrohityadav <cmrohityadav23@gmail.com>
2023-08-14 16:38:52 +0530

    for of

---
+++ 05_Loops/08_forof.js
@@ -0,0 +1,13 @@
+// ["", " "]
+// [{},{}]
+
+const arr=[1,2,3,4,5,6,7,8,9,]
+
+for (const x of arr){
+    console.log(x)
+}
+
+const greeting ="Hello World"
+for(const greet of greeting){
+    console.log(greet)
+}
