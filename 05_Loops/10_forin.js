dbe8b03a4ec0557abd861443d25666151645f54e
cmrohityadav <cmrohityadav23@gmail.com>
2023-08-14 16:39:04 +0530

    for in

---
+++ 05_Loops/10_forin.js
@@ -0,0 +1,36 @@
+const coder={
+    js:"javascript",
+    cpp:"c++",
+    pu:"python"
+}
+
+for (const key in coder) {
+//    console.log(key)
+}
+
+for (const key in coder) {
+    // console.log(coder[key])
+ }
+
+
+
+const lang=["c++","js","c#"];
+
+for (const key in lang) {
+    console.log(key)
+
+}
+
+
+
+const map=new Map()
+map.set("IN","India")
+map.set("USA","America")
+map.set("fr","France")
+map.set("IN","India")
+for (const key in map) {
+    console.log(key)
+
+}
+
+
