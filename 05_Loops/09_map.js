dc2e6328b198ec6059ef6011dc1608ae2cc847fa
cmrohityadav <cmrohityadav23@gmail.com>
2023-08-14 16:38:58 +0530

    map

---
+++ 05_Loops/09_map.js
@@ -0,0 +1,24 @@
+const map=new Map()
+map.set("IN","India")
+map.set("USA","America")
+map.set("fr","France")
+map.set("IN","India")
+
+// console.log(map)
+
+
+ for( const [key,value] of map){
+    // console.log(key, "value is ",value)
+ }
+
+ const hero={
+    game:"dora",
+    game2:"poki"
+ }
+//below code is wrong
+for (const [key,value] of hero) {
+    console.log(key,value)
+    
+}
+
+
