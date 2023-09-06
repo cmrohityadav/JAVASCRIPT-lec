533b5dfd1d6d3516ddda8e2014c1111cedae22f5
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-07 00:46:49 +0530

    Iterating over a Map

--- 05_Loops/09_map.js
+++ 05_Loops/09_map.js
@@ -6,19 +6,24 @@ map.set("IN","India")
 
 // console.log(map)
 
+for(const valuehai of map){
+   console.log(valuehai)
+}
 
  for( const [key,value] of map){
     // console.log(key, "value is ",value)
  }
+
+
 
  const hero={
     game:"dora",
     game2:"poki"
  }
 //below code is wrong
-for (const [key,value] of hero) {
-    console.log(key,value)
+// for (const [key,value] of hero) {
+//    //  console.log(key,value)
     
-}
+// }
 
 
