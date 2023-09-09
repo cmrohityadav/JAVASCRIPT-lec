49fc0fda55bd6bfad48a2d9891e5ff6a5380fbaf
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-10 01:45:17 +0530

    test

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
-let age=17;
-function name(age){
-    console.log("hii")
-    age++
-console.log(age)
+const promiseFour=new Promise(function(resolve,reject){
 
-}
+    setTimeout(function(){
+        let error=false;
+        if(!error){
+            resolve({username:"rohit",password:"123"})
+        }else{
+            reject("erro something went wrong")
+        }
 
-if(age>=18){
-    console.log("okay")
-}
-name(age)
-console.log(age)
+    },1000)
+})
+promiseFour.then(function(user){
+    console.log(user)
+    return user.username
+   
+}).then((username)=>{
+    console.log(username)
+
+}).catch(function(msg){
+console.log(msg)
+}).finally((use1)=>{
+    console.log("the promise is either reject or resolved")
+    console.log(use1)
+})
