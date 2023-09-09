e42ce9509ea00565d9cb754ee76f2d07eae32cf9
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-10 01:53:12 +0530

    test

--- test.js
+++ test.js
@@ -1,25 +1,6 @@
-const promiseFour=new Promise(function(resolve,reject){
+name();
 
-    setTimeout(function(){
-        let error=false;
-        if(!error){
-            resolve({username:"rohit",password:"123"})
-        }else{
-            reject("erro something went wrong")
-        }
 
-    },1000)
-})
-promiseFour.then(function(user){
-    console.log(user)
-    return user.username
-   
-}).then((username)=>{
-    console.log(username)
-
-}).catch(function(msg){
-console.log(msg)
-}).finally((use1)=>{
-    console.log("the promise is either reject or resolved")
-    console.log(use1)
-})
+function name(){
+    console.log("rohit")
+}
