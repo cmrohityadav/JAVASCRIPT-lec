5d2a5bc24db36eefd9d5625978efc2d1d40e5293
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-22 20:05:40 +0530

    Object literal

--- 20_OOPS/10_02_.js
+++ 20_OOPS/10_02_.js
@@ -11,12 +11,24 @@ function User(username, loginCount, isLoggedIn){
     return this
 }
 
+// const userOne=User("rohit",12,true)
+// const userTwo=User("rahul",500,false)
+
 const userOne=new User("rohit",12,true)
 const userTwo=new User("rahul",500,false)
-console.log(userTwo)
 console.log(userOne)
-console.log(userOne.constructor)
-/* 
+console.log(userTwo)
+// console.log(userOne.constructor)
+
+
+/* NOTES
+step1 : when we use the new keyword 
+it generate the new object instatance
+
+2: copy the all properties and methods
+
+3. inssert into this empty object
+
 
 
 */
