8bf9a26c9db82e05e3a89baef44f0d817e2e1fd7
cmrohityadav <cmrohityadav23@gmail.com>
2024-01-05 23:42:32 +0530

    oops

--- test.js
+++ test.js
@@ -1,2 +1,25 @@
-const characterIndex=Math.round(Math.random()*16);
-console.log(characterIndex)
+class Rail{
+book(){
+
+    console.log(`book-> name : ${this.name} train number : ${this.trainNo}`)
+}
+cancle(){
+    console.log(`cancle-> name : ${this.name} train number : ${this.trainNo}`)
+}
+fill(name,trainNo){
+    this.trainNo=trainNo;
+    this.name=name;
+
+
+}
+
+
+}
+
+let rohit=new Rail();
+
+rohit.fill("rohit",159);
+rohit.book();
+
+rohit.fill("yadav",456);
+rohit.book();
