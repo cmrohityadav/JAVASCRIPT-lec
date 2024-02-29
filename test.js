14208ba4f5e193af75da51a1b5e2dcf5266c065d
cmrohityadav <cmrohityadav23@gmail.com>
2024-02-29 20:23:09 +0530

    testing conecpt

--- test.js
+++ test.js
@@ -1,34 +1,15 @@
-class Animals{
-    constructor(sound,food){
-            this.sound=sound;
-            this.food=food;
-    }
-
-    awaj(){
-        console.log(`Animal ki awaj ${this.sound}`)
-    }
-    khana(){
-        console.log(`Animal ki khana ${this.food}`)
-
-    }
-
-}
-
-let cat =new Animals("meow","rat");
-cat.awaj();
-cat.khana();
-
-class bird extends Animals{
-
-    fly(){
-        console.log(`bird can fly`)
-    }
-    awaj(){
-        super.awaj()
-        console.log("this is bird voice")
+const req={
+    sis:{
+        name:"poonam"
     }
 }
 
-let gauriya =new bird("chhiichiii","insect");
-gauriya.fly()
-gauriya.awaj()
+const userdetail={
+    id:2,
+    name:"rohit"
+}
+
+
+const a=req.user=userdetail;
+console.log(req)
+console.log(req.user)
