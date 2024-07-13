953500ca1bf472e2873658ff81d4a54a0eacb638
cmrohityadav <cmrohityadav23@gmail.com>
2024-07-13 12:31:58 +0530

    day one added to 30dayChallenge : data and its type

---
+++ Practice/02_30DayChallenge/tasks.js
@@ -0,0 +1,49 @@
+console.log("// task1")
+
+var count=0;
+console.log(count);
+
+
+console.log("//task2")
+
+let fname="rohit";
+console.log(fname);
+
+
+console.log("//task3")
+
+const flag=true;
+console.log(flag);
+
+
+console.log("//task4")
+let rollNumber=40;
+let fName="rohit";
+let pass=true;
+let familyDetail={
+    fatherName:"C",
+    motherName:"M",  
+}
+let stock=["nse","bse","neft"];
+
+console.log(typeof(rollNumber));
+console.log(typeof(fname));
+console.log(typeof pass);
+console.log(typeof familyDetail);
+console.log(typeof stock);
+console.log(typeof stock[0]);
+
+
+console.log("task5");
+
+let surname="Yadav";
+console.log(surname);
+surname="Ahir";
+console.log(surname);
+
+
+console.log("//task 6");
+
+const pi=3.14;
+//pi=3.14159265 //it will throw error
+console.log(pi);
