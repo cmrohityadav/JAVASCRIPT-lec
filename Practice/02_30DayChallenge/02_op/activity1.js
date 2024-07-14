a4a979144143c852183955d5ffa7560912ca4f8d
cmrohityadav <cmrohityadav23@gmail.com>
2024-07-14 22:45:46 +0530

    day 2 operator task

---
+++ Practice/02_30DayChallenge/02_op/activity1.js
@@ -0,0 +1,94 @@
+console.log("//task1")
+let numOne=10;
+let numTwo=20;
+console.log(numOne+numTwo);
+
+console.log("//task2")
+let num3=20;
+let num4=5;
+console.log(num3-num4);
+
+console.log("//task3")
+let num5=10;
+let num6=100;
+console.log(num5*num6);
+
+console.log("//task4");
+let num7=100;
+let num8=50;
+console.log(num7/num8);
+
+console.log("//task5");
+let num10=5;
+let num11=2;
+console.log(num10%num11);
+
+console.log("//task6");
+let num30=10;
+ num30+=100;
+console.log(num30);
+
+console.log("//task7");
+let num40=20;
+num40-=10;
+console.log(num40);
+
+console.log("//task8");
+
+let num50=100;
+let num51=200;
+if(num50>num51){
+    console.log(`${num50} is greater number`);
+}else{
+    console.log(`${num51} is greater`);
+}
+
+console.log("//task9");
+let age=18;
+if(age>=18){
+    console.log("You can vote");
+}else{
+    console.log("u r child");
+}
+
+
+console.log("//task10");
+
+console.log(10=="10");
+console.log(10===10);
+
+
+
+console.log("//task11");
+
+
+let currentAge=20;
+let currentBalance=200;
+if(currentAge>=18 && currentBalance>=100){
+    console.log("you can enter ");
+}else if(currentAge>=18 && currentBalance<100 ){
+    console.log("do recharge ");
+}else{
+    console.log("u r kid");
+}
+
+console.log("//task12");
+
+let enterYourGuess=9;
+if(enterYourGuess==10 ||enterYourGuess== 11 ||enterYourGuess==12){
+    console.log("u r lucky one");
+}else{
+    console.log("try again");
+}
+
+
+console.log("//task13");
+console.log(!true);
+
+
+console.log("//task14");
+
+let num=10;
+num>0? console.log("positive"): console.log("negative");
+
+
