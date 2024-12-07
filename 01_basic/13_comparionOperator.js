6e02d50d3de189a66210a0dc71924cdb93a51606
Rohit <cmrohityadav@outlook.in>
2024-12-07 23:59:29 +0530

    added comparision

---
+++ 01_basic/13_comparionOperator.js
@@ -0,0 +1,9 @@
+let iOne=1;
+let iTwo=2;
+
+console.log("Equal to :",iOne==iTwo);
+console.log("Not Equal to :",iOne!=iTwo);
+console.log("Greater than :",iOne>iTwo);
+console.log("Greater than  Equal to :",iOne>=iTwo);
+console.log("Less than :",iOne<iTwo);
+console.log("Less than  Equal to :",iOne<=iTwo);
