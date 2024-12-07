826d406311bc4cb545d343b1a433cc2158d6698d
Rohit <cmrohityadav@outlook.in>
2024-12-07 23:56:10 +0530

    added arithmetic

---
+++ 01_basic/12_arithmeticOperators.js
@@ -0,0 +1,21 @@
+let iFive=5;
+let iTwo=2;
+
+let iAddidition=iFive+iTwo;
+
+let iSubtract=iFive-iTwo;
+
+let iMultiply=iTwo*iFive;
+
+let fDivision=iFive/iTwo;
+
+let iRemainder=iFive%iTwo;
+
+let iExponance=2**3;
+
+console.log(`addition of ${iFive} and ${iTwo} is ${iAddidition}`);
+console.log(`subtraction of ${iFive} and ${iTwo} is ${iSubtract}`);
+console.log(`Multiply of ${iFive} and ${iTwo} is ${iMultiply}`);
+console.log(`Division of ${iFive} and ${iTwo} is ${fDivision}`);
+console.log(`Remainder of ${iFive} and ${iTwo} is ${iRemainder}`);
+
