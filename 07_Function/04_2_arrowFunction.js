66e06b519c7b23449d8780ab326171af3dabaa2c
cmrohityadav <cmrohityadav23@gmail.com>
2024-12-23 20:58:35 +0530

    added the basic of HOF to basic file

--- 07_Function/04_2_arrowFunction.js
+++ 07_Function/04_2_arrowFunction.js
@@ -13,3 +13,11 @@ const add2=(num1,num2)=> (num1+num2);
 
 const add3=(num1,num2)=> ({name:"rohit"});
 
+const calculateTotal=(price,qty)=>{
+    return price*qty;
+
+}
+// below code is also fine :: just write in one single line
+// calculateTotal=(price,qty)=>return price*qty;
+let totalCost=calculateTotal(10,50)
+console.log(totalCost)
