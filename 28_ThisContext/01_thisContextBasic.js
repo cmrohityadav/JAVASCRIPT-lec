74aa3c0f37e3a2fc114e3b638d602ba40928cc11
cmrohityadav <cmrohityadav23@gmail.com>
2024-12-30 00:44:07 +0530

    added this

---
+++ 28_ThisContext/01_thisContextBasic.js
@@ -0,0 +1,15 @@
+const person={
+    name:"Rohit",
+    greet:function(){
+        console.log(`hi , I am ${this.name}`)
+    }
+}
+
+person.greet();
+
+//now here will context is loss
+const greetFunction=person.greet
+greetFunction() // here undefined bcz context is loss
+
+const boundGreet=person.greet.apply.bind({name:"rahul"})
+console.log(boundGreet())
