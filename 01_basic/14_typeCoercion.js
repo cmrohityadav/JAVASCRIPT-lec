132460ad6ef4abaa3eab72ebf499ee4843fe499c
cmrohityadav <cmrohityadav23@gmail.com>
2025-02-24 23:08:02 +0530

    added concept type Coercion

---
+++ 01_basic/14_typeCoercion.js
@@ -0,0 +1,22 @@
+/**
+ * 1.Always follow BODMAS 
+ * 2.function of '+' is addition and concetaenation
+ * 3. + => additon only if both side of it is number
+ * 4. + => do concentation if any of is string 
+ * 5. other operator like -,*,/ treat as number
+ */
+
+let a=10;
+let b=20;
+
+console.log("Sum of 10 n 20 : "+(a+b))
+console.log("Sum of 10 n 20 : "+a+b)
+console.log(a+b+" : Sum of 10 n 20 : ")
+
+let c=100;
+let d="200"
+console.log(c+d)
+console.log(c*d)
+console.log(c-d)
+
+
