afd5fff067f1528634ff428610eb16a06ee6f8ca
cmrohityadav <cmrohityadav23@gmail.com>
2025-03-01 23:31:46 +0530

    all about ternary operator

---
+++ 02_ConditonalStatements/17_ternaryOperator.js
@@ -0,0 +1,8 @@
+// Ternary Operator =>  ? :
+//  
+
+console.log(210>10?"Yes u r right TRUE":"WRONG FALSE")
+
+// nested ternary operator
+let num=25
+console.log(num>0? "Positive":num<0?"Negative":"Zero")
